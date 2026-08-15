a2d36728f50ea1e46edccabb6242ed8df6331949
wscott2k26 <loftlatte25@gmail.com>
2026-08-15 17:23:17 -0400

    feat: report React mount to native shell

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
+import { installNativeBootErrorForwarding, reportNativeBootReady } from '@/lib/nativeBoot'
 import '@/index.css'
+
+installNativeBootErrorForwarding()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
 )
+
+if (typeof window !== 'undefined' && typeof window.requestAnimationFrame === 'function') {
+  window.requestAnimationFrame(() => reportNativeBootReady())
+} else {
+  reportNativeBootReady()
+}
